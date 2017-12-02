88280f7a71fb0134e498188e170badd3f385ae8d
Bennett Jacobs <bjacobs@pio.carrollu.edu>
2017-12-02 16:47:53 -0600

    Created FileHandler

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,6 +2,10 @@ import controller.DatabaseController;
 import controller.StateController;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import util.FileHandler;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by Jacob on 11/6/2017.
@@ -18,11 +22,10 @@ public class Driver extends Application{
 
 
     public static void main(String args[]){
+        launch(args);
 
-        //launch(args);
-
-        DatabaseController db = new DatabaseController();
-        db.executeSql(db.getChocAnMemberValidation(102685542));
+        //DatabaseController db = new DatabaseController();
+        //db.executeSql(db.getChocAnMemberValidation(102685542));
     }
 
 }
--- src/main/java/util/FileHandler.java
+++ src/main/java/util/FileHandler.java
@@ -1,4 +1,66 @@
 package util;
 
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class FileHandler {
+
+    //@TODO: Probably want to pass in List of reports
+    public void writeMemberReport(String report, String memName, int memID){
+        LocalDate ld = LocalDate.now();
+        String fileName = memName + "-" + memID + "_" + DateTimeFormatter.ofPattern("MM-dd-yy").format(ld) + ".txt";
+        File file = new File("reports/members/" + fileName);
+
+        try{
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write(report);
+            bw.close();
+        }
+        catch(IOException ex){
+            System.out.println("File could not be created");
+        }
+
+    }
+
+    public void writeProviderReport(String report, String provName, int provID){
+        LocalDate ld = LocalDate.now();
+        String fileName = provName + "-" + provID + "_" + DateTimeFormatter.ofPattern("MM-dd-yy").format(ld) + ".txt";
+        File file = new File("reports/providers/" + fileName);
+
+        try{
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write(report);
+            bw.close();
+        }
+        catch(IOException ex){
+            System.out.println("File could not be created");
+        }
+    }
+
+    public void writeManagerReport(String report){
+        LocalDate ld = LocalDate.now();
+        String fileName = "ManagerReport_" + DateTimeFormatter.ofPattern("MM-dd-yy").format(ld) + ".txt";
+        File file = new File("reports/managers/" + fileName);
+
+        try{
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write(report);
+            bw.close();
+        }
+        catch(IOException ex){
+            System.out.println("File could not be created");
+        }
+    }
+
+
+
+
+
 }
