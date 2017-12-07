21724d58072fbbb2dcb51185eb55d44f9cb78f78
bkrumms <bekrumms@gmail.com>
2017-12-06 18:11:41 -0600

    Added in the file handler calls into the report controller

--- src/main/java/controller/ReportController.java
+++ src/main/java/controller/ReportController.java
@@ -37,19 +37,24 @@ public class ReportController {
           System.out.println("No report to generate.");
           return;
         }
-        rs.next();
-        String memberReport = rs.getString("mem.mem_name");
-        memberReport += String.format("\n%d", rs.getInt("mem.mem_id"));
-        memberReport += String.format("\n%s", rs.getString("mem.mem_addr"));
-        memberReport += String.format("\n%s", rs.getString("mem.mem_city"));
-        memberReport += String.format("\n%s", rs.getString("mem.mem_state"));
-        memberReport += String.format("\n%s", rs.getInt("mem.mem_zip"));
-        memberReport += "\nServices received:";
-        do{
-          memberReport += String.format("\n\t%s", rs.getDate("servhs.serv_dte"));
-          memberReport += String.format("\n\t%s", rs.getString("prov.prov_name"));
-          memberReport += String.format("\n%s", rs.getString("serv.serv_name"));
-        }while(rs.next());
+
+        while(rs.next()) {
+          String memName = rs.getString("mem.mem_name");
+          int memId = rs.getInt("mem.mem_id");
+          String memberReport = rs.getString("mem.mem_name");
+          memberReport += String.format("\n%d", rs.getInt("mem.mem_id"));
+          memberReport += String.format("\n%s", rs.getString("mem.mem_addr"));
+          memberReport += String.format("\n%s", rs.getString("mem.mem_city"));
+          memberReport += String.format("\n%s", rs.getString("mem.mem_state"));
+          memberReport += String.format("\n%s", rs.getInt("mem.mem_zip"));
+          memberReport += "\nServices received:";
+          do{
+            memberReport += String.format("\n\t%s", rs.getDate("servhs.serv_dte"));
+            memberReport += String.format("\n\t%s", rs.getString("prov.prov_name"));
+            memberReport += String.format("\n%s", rs.getString("serv.serv_name"));
+          }while(rs.next() && memName.compare(rs.getString("mem.mem_name")));
+          fh.writeMemberReport(memberReport, memName, memId);
+        }
     }
 
     private void createManagerReport(ResultSet rs) throws SQLException {
@@ -75,6 +80,7 @@ public class ReportController {
         managerReport += String.format("\nTotal number of providers: %d", numOfProv);
         managerReport += String.format("\nTotal number of consultations: %d", numOfConsul);
         managerReport += String.format("\nTotal service fee: %d", overallFeeTot);
+        fh.writeManagerReport(managerReport);
     }
 
     private void createProviderReport(ResultSet rs) throws SQLException {
@@ -84,25 +90,29 @@ public class ReportController {
           return;
         }
         int totalFee = 0, totalConsultations = 0;
-        rs.next();
-        String providerReport = rs.getString("prov.prov_name");
-        providerReport += String.format("\n%d", rs.getInt("prov.prov_id"));
-        providerReport += String.format("\n%s", rs.getString("prov.prov_addr"));
-        providerReport += String.format("\n%s", rs.getString("prov.prov_city"));
-        providerReport += String.format("\n%s", rs.getString("prov.prov_state"));
-        providerReport += String.format("\n%s", rs.getInt("prov.prov_zip"));
-        providerReport += "\nServices provided:";
-        do{
-          providerReport += String.format("\n\t%s", rs.getDate("servhs.serv_dte"));
-          providerReport += String.format("\n\t%s", rs.getDate("servhs.tim_stmp"));
-          providerReport += String.format("\n\t%s", rs.getString("mem.mem_name"));
-          providerReport += String.format("\n\t%s", rs.getInt("serv.serv_id"));
-          providerReport += String.format("\n\t%d", rs.getInt("servhs.serv_fee"));
-          totalFee += rs.getInt("servhs.serv_fee");
-          totalConsultations++;
-        }while(rs.next());
-        providerReport += String.format("\nTotal number of consultations: %s", totalConsultations);
-        providerReport += String.format("\nTotal fee for the week: %s", totalFee);
+        while(rs.next()) {
+          String proName = rs.getString("prov.prov_name");
+          int proId = rs.getInt("prov.prov_id");
+          String providerReport = rs.getString("prov.prov_name");
+          providerReport += String.format("\n%d", rs.getInt("prov.prov_id"));
+          providerReport += String.format("\n%s", rs.getString("prov.prov_addr"));
+          providerReport += String.format("\n%s", rs.getString("prov.prov_city"));
+          providerReport += String.format("\n%s", rs.getString("prov.prov_state"));
+          providerReport += String.format("\n%s", rs.getInt("prov.prov_zip"));
+          providerReport += "\nServices provided:";
+          do{
+            providerReport += String.format("\n\t%s", rs.getDate("servhs.serv_dte"));
+            providerReport += String.format("\n\t%s", rs.getDate("servhs.tim_stmp"));
+            providerReport += String.format("\n\t%s", rs.getString("mem.mem_name"));
+            providerReport += String.format("\n\t%s", rs.getInt("serv.serv_id"));
+            providerReport += String.format("\n\t%d", rs.getInt("servhs.serv_fee"));
+            totalFee += rs.getInt("servhs.serv_fee");
+            totalConsultations++;
+          }while(rs.next() && proName.compare(rs.getString("prov.prov_name")));
+          providerReport += String.format("\nTotal number of consultations: %s", totalConsultations);
+          providerReport += String.format("\nTotal fee for the week: %s", totalFee);
+          fh.writeProviderReport(providerReport, proName, proId);
+        }
     }
 
 
